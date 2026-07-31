a0f95d88c555300b5117c26cccab625a933f9cb8
Your Name <you@example.com>
2026-07-31 15:48:35 +0300

    ads changes

--- resources/views/layouts/vertical.blade.php
+++ resources/views/layouts/vertical.blade.php
@@ -30,7 +30,15 @@
     </style>
 
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18160580545"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'AW-18160580545');
+</script>
 <body>
 
     <!-- Begin page -->
