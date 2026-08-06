c3e6f1e174acfdef6578cea6f8a9e01f61192b33
Your Name <you@example.com>
2026-08-06 08:38:34 +0300

    new email

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://www.irish-manualhandling.com/'),
+    'url' => env('APP_URL', 'https://www.ireland-safetycourse.com/'),
 
     'asset_url' => env('ASSET_URL'),
 
