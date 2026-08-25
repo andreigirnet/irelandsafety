db2a254189d51f74b09e29478c6b52d754f0b539
Your Name <you@example.com>
2026-08-25 10:04:42 +0300

    google tag

--- resources/views/layouts/vertical.blade.php
+++ resources/views/layouts/vertical.blade.php
@@ -2,13 +2,6 @@
 <html lang="en" data-sidenav-size="{{ $sidenav ?? 'default' }}" data-layout-mode="{{ $layoutMode ?? 'fluid' }}" data-layout-position="{{ $position ?? 'fixed' }}" data-menu-color="{{ $menuColor ?? 'dark' }}" data-topbar-color="{{ $topbarColor ?? 'light' }}">
 
 <head>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-N4X9SGX6');</script>
-    <!-- End Google Tag Manager -->
     @include('layouts.shared/title-meta', ['title' => $title])
     @yield('css')
     @include('layouts.shared/head-css', ['mode' => $mode ?? '', 'demo' => $demo ?? ''])
@@ -45,11 +38,15 @@
 <!-- Event snippet for Achiziție (4) conversion page -->
 
 <body>
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18160580545">
+</script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4X9SGX6"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+    gtag('config', 'AW-18160580545');
+</script>
     <!-- Begin page -->
     <div class="wrapper">
 
