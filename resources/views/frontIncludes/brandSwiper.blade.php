99e77fb74bf511f8d5a3aa079c25e4330d2df058
Your Name <you@example.com>
2026-08-30 19:27:02 +0300

    new changes

--- resources/views/frontIncludes/brandSwiper.blade.php
+++ resources/views/frontIncludes/brandSwiper.blade.php
@@ -32,6 +32,6 @@
 </div>
 <div class="copyright">
     <p style="color: white; font-size: 18px">
-        Copyright © 2026 IrelandSafetyCourse.com | Safety Ireland Course Ltd | Email: info@IrelandSafetyCourse.com
+        Copyright © 2026 IrelandSafetyCourse.com | PROGRESSIVE VELOCITY FAST SOLUTIONS LIMITED 822301 Ltd | Email: info@IrelandSafetyCourse.com
     </p>
 </div>
